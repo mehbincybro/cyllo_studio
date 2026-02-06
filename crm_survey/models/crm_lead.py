fc42df6719c60bc948b3488e5ac514e698fad38b
Roshin-pk <roshinshraa@gmail.com>
2026-02-06 18:23:32 +0530

    [UPDT]I have created a button to link the Survey module with CRM. Using this button, users can create a custom survey for a client and easily send it to the customer.

---
+++ crm_survey/models/crm_lead.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cyllo Pvt. Ltd.
+#
+#    Copyright (C) 2025-TODAY Cyllo(<https://www.cyllo.com>)
+#    Author: Cyllo(<https://www.cyllo.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import models
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+
+
+
+    def action_create_survey(self):
+        """Action to create the survey."""
+        print(type(self.id))
+        return {
+            'type': 'ir.actions.act_window',
+            'res_model': 'survey.survey',
+            'view_mode': 'form, kanban',
+            'views': [
+                (self.env.ref('survey.survey_survey_view_kanban').id, 'kanban'),
+                (self.env.ref('survey.survey_survey_view_form').id, 'form'),
+            ],
+            'target': 'current',
+            'context': {
+                'default_name': f'{self.display_name} Survey',
+                'default_lead_id': self.id,
+            },
+
+        }
